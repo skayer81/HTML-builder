665125e335885ced2d88c711bf218657ba099652
Sergey <seleznev-krista@mail.ru>
2024-01-22 01:09:06 +0500

    feat: add 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const readdir = require('node:fs/promises');
+const path = require('node:path');
+readdir
+  .readdir(path.resolve(__dirname, 'secret-folder'), { withFileTypes: true })
+  .then((result) => {
+    for (const file of result) {
+      if (file.isFile()) {
+        readdir
+          .stat(path.resolve(__dirname, 'secret-folder', file.name))
+          .then((stat) => {
+            console.log(`${String(file.name).slice(
+                  0,
+                  String(file.name).lastIndexOf('.'),
+                )} - ${String(path.extname(file.name)).slice(1)} - ${
+                  stat.size
+                }b`);
+          });
+      }
+    }
+  });
